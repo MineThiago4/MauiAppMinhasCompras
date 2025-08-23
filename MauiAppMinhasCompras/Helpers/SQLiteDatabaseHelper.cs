99047b478bbc86389bdab593b15c2b3c18d55118
MineT4 <minethiago4@gmail.com>
2025-08-23 16:17:36 -0300

    Adicionado a lógica para inserção de dados e desenvolvido o esqueleto da página "Novo Produto"

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -7,37 +7,38 @@ namespace MauiAppMinhasCompras.Helpers
     {
 
         readonly SQLiteAsyncConnection _conn;
-        public SQLiteDatabaseHelper(string path) 
+        public SQLiteDatabaseHelper(string path)
         {
             _conn = new SQLiteAsyncConnection(path);
             _conn.CreateTableAsync<Produto>().Wait();
         }
 
-        public Task<int> Insert(Produto p) {
-        
+        public Task<int> Insert(Produto p)
+        {
+
             return _conn.InsertAsync(p);
         }
 
-        public Task<List<Produto>> Update(Produto p) 
+        public Task<List<Produto>> Update(Produto p)
         {
             string sql = "UPDATE Produto SET Descricao = ?, Quantidade = ?, Preco = ? WHERE Id = ?";
             return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
         }
 
-        public Task<int> Delete(int Id) 
+        public Task<int> Delete(int Id)
         {
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == Id);
         }
 
-        public Task<List<Produto>> GetAll() 
+        public Task<List<Produto>> GetAll()
         {
             return _conn.Table<Produto>().ToListAsync();
         }
 
-        public Task<List<Produto>> Search(string p) 
+        public Task<List<Produto>> Search(string p)
         {
-            string sql = "SELECT * Produto Where descricao LIKE '%" +p+ "%'";
+            string sql = "SELECT * Produto Where descricao LIKE '%" + p + "%'";
             return _conn.QueryAsync<Produto>(sql);
         }
-    }         
+    }
 }
